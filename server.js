111ea197cca078c57f8f8ebfcd20808001882432
Tony Erwin <aerwin@us.ibm.com>
2014-09-02 23:55:24 -0400

    Clean-up server.js comments, remove commented out line from index.html, and update .cfignore so Eclipse setting not uploaded on cf push

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
 /* Copyright IBM Corp. 2014 All Rights Reserved                      */
 
-// Bring in the Express framework
+// Require and create the Express framework
 var express = require('express');
 var app = express();
 
-// Determine port our app will run on
+// Determine port to listen on
 var port = (process.env.VCAP_APP_PORT || 3000);
 
-// We want to enable the reverse proxy support in
-// Express: http://expressjs.com/api#app-settings
+// Enable reverse proxy support in Express. This causes the
+// the "X-Forwarded-Proto" header field to be trusted so its
+// value can be used to determine the protocol. See 
+// http://expressjs.com/api#app-settings for more details.
 app.enable('trust proxy');
 
-// Add a handling to inspect the req.secure flag (see 
-// http://expressjs.com/api#req.secure) to know whether 
-// the request was via http or https.
+// Add a handler to inspect the req.secure flag (see 
+// http://expressjs.com/api#req.secure). This allows us 
+// to know whether the request was via http or https.
 app.use (function (req, res, next) {
 	if (req.secure) {
-		// request was via https, so do no
-		// special handling
+		// request was via https, so do no special handling
 		next();
 	} else {
-		// request was via http, so redirect to
-		// https
+		// request was via http, so redirect to https
 		res.redirect('https://' + req.headers.host + req.url);
 	}
 });
 
-// Allow static files from the public directory to be served
+// Allow static files in the /public directory to be served
 app.use(express.static(__dirname + '/public'));
 
+// Start listening on the port
 var server = app.listen(port, function() {
 	console.log('Listening on port %d', server.address().port);
 });
